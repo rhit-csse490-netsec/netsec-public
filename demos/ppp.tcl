06c1bbc61141c659b60eb06dc223e7955a3f7758
Mohammad N <noureddi@rose-hulman.edu>
2022-08-26 11:40:33 -0400

    updated to Ubuntu 20.04 as the default OS

--- demos/ppp.tcl
+++ demos/ppp.tcl
@@ -4,8 +4,8 @@ source tb_compat.tcl
 set server [$ns node]
 set client [$ns node]
 
-tb-set-node-os $server 490_ubuntu_1804
-tb-set-node-os $client 490_ubuntu_1804
+tb-set-node-os $server Ubuntu2004-STD
+tb-set-node-os $client Ubuntu2004-STD
 
 set lan0 [$ns make-lan "$server $client" 1000Mb 0ms]
 
